a0734ae591c97f419e8cefadcc43e4e3d7385f6b
HampusEriksson <hampus.eriksson@ga.ntig.se>
2023-12-30 20:04:37 +0100

    Challenge 3 done

---
+++ Challenge 3/Assets/Challenge 3/Scripts/RepeatBackgroundX.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatBackgroundX : MonoBehaviour
+{
+    private float width;
+
+    private Vector3 respawnPos ;
+
+    private void Start()
+    {
+        width = GetComponent<BoxCollider>().size.x; // Set repeat width to half of the background
+        respawnPos = new Vector3(width, 9.5f, 4);
+    }
+
+    private void Update()
+    {
+        // If background moves left by its repeat width, move it back to start position
+        if (transform.position.x < -width)
+        {
+            transform.position = respawnPos;
+        }
+    }
+
+ 
+}
+
+
